f31c4028116497df7a2dff713e6a289d29cf59a1
grayross01 <grayross01@gmail.com>
2025-10-24 21:26:17 -0700

    Add Google Analytics tracking (G-BZNV0WEVZS)

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BZNV0WEVZS"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BZNV0WEVZS');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
